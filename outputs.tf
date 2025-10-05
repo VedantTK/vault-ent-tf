ee2dd5d2c18e7578e5a3661661a5f6eacaa331f9
Vedant Kocharekar <110671057+VedantTK@users.noreply.github.com>
2025-10-05 13:14:23 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "name" {
+  value = docker_container.vault_enterprise.name
+}
+
+output "image" {
+  value = docker_container.vault_enterprise.image
+}
+output "ports" {
+  value = docker_container.vault_enterprise.ports
+}
+
+output "id" {
+  value = docker_container.vault_enterprise.id
+}
